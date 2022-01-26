ca64b21b6529365af3302b8e238f9933ef00529a
Oguz <yilmazoguz@outlook.com>
2022-01-26 13:07:37 +0300

    Remove images folder since lorem picsum will be utilized.

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Image() {
     return (
         <div>
-            <img src="" alt="" />
+            <img src="https://picsum.photos/416/374" alt="random image as lorem picsum" />
         </div>
   )
 }
