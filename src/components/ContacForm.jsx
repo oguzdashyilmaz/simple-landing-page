f13673c5932d92518db5308ee5933513fd06b993
Oguz <yilmazoguz@outlook.com>
2022-01-26 14:34:18 +0300

    Create the component of ContactForm and add in Contact.js.

---
+++ src/components/ContacForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ContacForm() {
+  return <div></div>;
+}
+
+export default ContacForm;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ContacForm from './ContacForm';
 
 function Contact() {
   return (
     <div>
         <h3>Contact us to get started</h3>
         <p>We'll reply when we get back from fishing.</p>
+        <ContacForm/>
     </div>
   )
 }
