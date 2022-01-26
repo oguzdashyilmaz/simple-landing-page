d9bcdff2f1e13e733a595650d9fbd6217508e991
Oguz <yilmazoguz@outlook.com>
2022-01-26 15:21:41 +0300

    Remove outline of inputs.

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -5,27 +5,27 @@ import { ReactDOM } from 'react';
 function ContactForm() {
     return (
         <form>
-            <div className="flex-row">
+            <div className="flex-row bg-color__contactform">
                 <label>
                     Name<br/>
                     <input type="text" name="name" className='input-box'/>
                 </label>
                 <label>
                     Email<br/>
-                    <input type="text" name="email" />
+                    <input type="text" name="email" className='input-box'/>
                 </label>
             </div>
             <div className="flex-col">
                 <label>
                     Timeline<br/>
-                    <input type="text" name="timeline" />
+                    <input type="text" name="timeline" className='input-box'/>
                 </label>
                 <label>
                     Message<br/><br/>
-                    <input type="text" name="message" />
+                    <input type="text" name="message" className='input-box'/>
                 </label>
             </div>
-            <input type="submit" value="Send" className='text-right'/>
+            <input type="submit" value="Send" className='text-right input-box'/>
         </form>
     )
 }
