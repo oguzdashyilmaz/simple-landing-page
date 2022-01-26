654565fec2e18cdd2f8656c057d2a834ed79976f
Oguz <yilmazoguz@outlook.com>
2022-01-26 13:02:39 +0300

    Add h2 and p with lorem20 in Text component.

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 function Text() {
     return (
         <div>
-
+            <h2>Lorem, ipsum dolor.</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A ratione et dolor eum aperiam voluptas veritatis praesentium nobis omnis vel!</p>
         </div>
     );
 }
