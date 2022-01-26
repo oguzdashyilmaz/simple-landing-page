b6d571f27c7669fa686f0af9c65668a0e15f4061
Oguz <yilmazoguz@outlook.com>
2022-01-26 13:15:53 +0300

    Add h5 and h1 in the component of Header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 function Header() {
     return (
         <div>
-
+            <h5>Welcome to</h5>
+            <h1>Welcome to Camp Chippewa</h1>
         </div>
     );
 }
