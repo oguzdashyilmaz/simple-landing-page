e4d1e59e2586ac964356a320a865e114da7f93ea
Oguz <yilmazoguz@outlook.com>
2022-01-26 13:00:25 +0300

    Add all components accordingly in App.js.

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row flex-row">
+        <Image />
+        <Text />
+      </div>
+      <div className="row">
+        <Text />
+        <Image />
+      </div>
+      <div className="row">
+        <Image />
+        <Text />
+      </div>
+      <div className="row">
+        <Text />
+        <Image />
+      </div>
+      <div className="row">
+        <Image />
+        <Text />
+      </div>
     </div>
   );
 }
