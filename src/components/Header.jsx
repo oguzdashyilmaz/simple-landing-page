ccc973323907458a17733df6bb14802f609bb210
Oguz <yilmazoguz@outlook.com>
2022-01-26 13:18:16 +0300

    Create and add css class of text-center to center h5 and h1.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
     return (
-        <div>
+        <div className='text-center'>
             <h5>Welcome to</h5>
             <h1>Welcome to Camp Chippewa</h1>
         </div>
