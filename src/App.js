381dcd834f6f97e1e24a46406fb050416a6584c8
Oguz <yilmazoguz@outlook.com>
2022-01-26 13:16:42 +0300

    Import the component of Header in App.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Text from './components/Text';
 import Image from './components/Image';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <div className="row flex-row">
         <Image />
         <Text />
