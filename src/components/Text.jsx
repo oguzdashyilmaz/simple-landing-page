a29f62675d6242c3a144ec609581273542f720e8
Oguz <yilmazoguz@outlook.com>
2022-01-26 13:03:53 +0300

    Give className of text-container to be covering both h2 and p with lorem20 in Text component.

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Text() {
     return (
-        <div>
+        <div className='text-container'>
             <h2>Lorem, ipsum dolor.</h2>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A ratione et dolor eum aperiam voluptas veritatis praesentium nobis omnis vel!</p>
         </div>
