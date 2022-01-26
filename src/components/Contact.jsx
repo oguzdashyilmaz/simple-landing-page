f479374adf5eddb5f1f1c355641f1938c93522f7
Oguz <yilmazoguz@outlook.com>
2022-01-26 14:32:50 +0300

    Create the component of Contact.

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+        <h3>Contact us to get started</h3>
+        <p>We'll reply when we get back from fishing.</p>
+    </div>
+  )
+}
+
+export default Contact;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 
 function Footer() {
-  return (
-    <div>
-        <p>W5723 Lake View Chester, CA 96020</p>
-        <p>Design by <em>Papaya</em></p>
-        <div className="social-icons text-center">
-            <i class="fab fa-facebook "></i>
-            <i class="fab fa-instagram "></i>
-            <i class="fab fa-twitter "></i>
+    return (
+        <div>
+            <p>W5723 Lake View Chester, CA 96020</p>
+            <p>Design by <em>Papaya</em></p>
+            <div className="social-icons text-center">
+                
+            </div>
+            <p>&copy; Camp Chippewa</p>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Footer;
